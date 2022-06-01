71c2ee917f61695fa0b02277d1f90a3164a7374e
HongRok Kim <hlog2e@gmail.com>
2022-06-02 01:24:23 +0900

    Add Google Analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,23 @@
 import "../styles/globals.css";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      console.log(gtag);
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,9 +1,27 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import * as gtag from "../lib/gtag";
 
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
