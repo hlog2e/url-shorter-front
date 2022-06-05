675bfd7da199577a275f34022cc483b351629988
HongRok Kim <hlog2e@gmail.com>
2022-06-05 17:25:48 +0900

    Add react-reveal for scroll animation

--- components/UrlLogger/UrlLoggerContainer.jsx
+++ components/UrlLogger/UrlLoggerContainer.jsx
@@ -1,19 +1,22 @@
 import UrlLoggerItem from "./UrlLoggerItem";
+import Fade from "react-reveal/Fade";
 
 export default function UrlLoggerContainer(props) {
   return (
-    <div className="flex flex-col  mt-5  max-w-[550px]">
-      {props.localStorage ? (
-        <>
-          {props.localStorage.map((item) => (
-            <UrlLoggerItem
-              key={item.short_url}
-              long_url={item.long_url}
-              short_url={item.short_url}
-            />
-          ))}
-        </>
-      ) : null}
-    </div>
+    <Fade bottom>
+      <div className="flex flex-col  mt-5  max-w-[550px]">
+        {props.localStorage ? (
+          <>
+            {props.localStorage.map((item) => (
+              <UrlLoggerItem
+                key={item.short_url}
+                long_url={item.long_url}
+                short_url={item.short_url}
+              />
+            ))}
+          </>
+        ) : null}
+      </div>
+    </Fade>
   );
 }
