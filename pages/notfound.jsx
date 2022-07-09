c00d45a44adbb5d3afa4373cb993ae9ad3b815ff
HongRok Kim <hlog2e@gmail.com>
2022-07-09 11:59:08 +0900

    Update 404 Page

--- pages/notfound.jsx
+++ pages/notfound.jsx
@@ -8,15 +8,23 @@ export default function NotFound() {
         <title>GOURL | Not Found | 404</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <div className="flex flex-col items-center justify-center text-white bg-blue-600 h-2/5">
-        <div className="flex flex-col items-center ">
-          <h1 className="text-4xl font-semibold md:text-6xl">404 Not Found</h1>
-          <h1 className="ml-5 text-3xl font-semibold md:text-6xl">
-            존재하지 않는 URL 입니다.
-          </h1>
+      <div className="flex items-center justify-center h-screen">
+        <img className="w-96" src="/notfound.svg" />
+        <div className="flex flex-col justify-between h-40 ml-32 w-[500px]">
+          <div>
+            <h1 className="text-2xl font-jalnan">GOURL</h1>
+            <h1 className="text-4xl font-bold text-slate-500">404 Not Found</h1>
+          </div>
+          <div>
+            <p className="text-xl font-semibold ">
+              URL이 잘못되었거나, 이 세상에 존재하지 않습니다.
+            </p>
+            <p className="mt-1 font-semibold text-slate-400">
+              URL 링크를 다시 한 번 확인해주세요.
+            </p>
+          </div>
         </div>
       </div>
-      <DivWaves className="w-full  h-10 md:h-28 relative top-[-1px] " />
     </div>
   );
 }
